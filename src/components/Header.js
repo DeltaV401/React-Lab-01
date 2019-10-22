80f1e8421629793938d63eadbe799555e7fc884f
Jon Struve <ocdammo3@gmail.com>
2019-10-22 16:19:45 -0500

    Converted Header to ES6

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Happy Halloween</h1>
-    </header>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Happy Halloween</h1>
+      </header>
+    )  
+  };
 };
 
 export default Header;
